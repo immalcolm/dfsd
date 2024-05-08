a3727235c424bd2c493a48604417f7c4f0fa77bd
immalcolm <malcolm_yam@np.edu.sg>
2024-05-08 19:41:42 +0800

    demo on q7 on live vs static nodes in tutorial 5

--- exercises/tutorial05/tutorial05-students/ex07/script.js
+++ exercises/tutorial05/tutorial05-students/ex07/script.js
@@ -9,3 +9,16 @@ in the loop
 --> modify the className to cool
 */
 //console.log("hi");
+
+//Write code using a loop to select only the elements with the class li.hot and change all the class attributes of the elements to cool.
+
+let hot = document.querySelectorAll(".hot");
+//let hot = document.getElementsByClassName("hot");
+
+console.log(`Number of hot ${hot.length}`);
+
+for(let i = 0; i< hot.length;i++){
+    hot[i].classList.add("cool");
+    hot[i].classList.remove("hot");
+   
+}
